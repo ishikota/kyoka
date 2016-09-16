d01f0fef3c101034a8b70b8483ffc0b0807f2c3d
Kota Ishimoto <ishikota086@gmail.com>
2016-09-16 17:53:02 +0900

    Create base domain class

---
+++ kyoka/domain/base_domain.py
@@ -0,0 +1,37 @@
+class BaseDomain:
+
+  def __init__(self):
+    pass
+
+  def generate_inital_state(self):
+    err_msg = self.__build_err_msg("generate_inital_state")
+    raise NotImplementedError(err_msg)
+
+  def generate_initial_Q_table(self):
+    err_msg = self.__build_err_msg("generate_initial_Q_table")
+    raise NotImplementedError(err_msg)
+
+  def is_terminal_state(self, state):
+    err_msg = self.__build_err_msg("is_terminal_state")
+    raise NotImplementedError(err_msg)
+
+  def transit_state(self, state, action):
+    err_msg = self.__build_err_msg("transit_state")
+    raise NotImplementedError(err_msg)
+
+  def generate_possible_actions(self, state):
+    err_msg = self.__build_err_msg("generate_possible_actions")
+    raise NotImplementedError(err_msg)
+
+  def calculate_reward(self, state):
+    err_msg = self.__build_err_msg("calculate_reward")
+    raise NotImplementedError(err_msg)
+
+  def fetch_Q_value(self, Q_table, state, action):
+    err_msg = self.__build_err_msg("fetch_Q_value")
+    raise NotImplementedError(err_msg)
+
+
+  def __build_err_msg(self, msg):
+    return "Your client does not implement [ {0} ] method".format(msg)
+
