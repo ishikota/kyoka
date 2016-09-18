6df88da31869737f634cb17593db6caae1fc88e4
Kota Ishimoto <ishikota086@gmail.com>
2016-09-18 17:18:03 +0900

    Use BaseTableValueFunction class in ticktacktoe

--- sample/ticktacktoe/ticktacktoe_table_value_function.py
+++ sample/ticktacktoe/ticktacktoe_table_value_function.py
@@ -1,31 +1,26 @@
 import math
-from kyoka.algorithm.value_function.base_action_value_function import BaseActionValueFunction
+from kyoka.algorithm.value_function.base_table_action_value_function import BaseTableActionValueFunction
 
-class TickTackToeTableValueFunction(BaseActionValueFunction):
+class TickTackToeTableValueFunction(BaseTableActionValueFunction):
 
-  def calculate_value(self, state, action):
-    first_player_board, second_player_board = state
-    move_position = int(math.log(action,2))
-    Q_value = self.Q_table[first_player_board][second_player_board][move_position]
-    return Q_value
-
-  def update_function(self, state, action, new_value):
-    first_player_board, second_player_board = state
-    move_position = int(math.log(action,2))
-    delta = new_value - self.Q_table[first_player_board][second_player_board][move_position]
-    self.Q_table[first_player_board][second_player_board][move_position] = new_value
-    return delta
-
-  def setUp(self):
-    self.Q_table = self.__generate_initial_Q_table()
-
-
-  def __generate_initial_Q_table(self):
+  def generate_initial_table(self):
     board_state_num = 2**9
     action_num = 9
     Q_table = [[[0 for a in range(action_num)]\
         for j in range(board_state_num)] for i in range(board_state_num)]
     return Q_table
+
+  def fetch_value_from_table(self, table, state, action):
+    first_player_board, second_player_board = state
+    move_position = int(math.log(action,2))
+    Q_value = table[first_player_board][second_player_board][move_position]
+    return Q_value
+
+  def update_table(self, table, state, action, new_value):
+    first_player_board, second_player_board = state
+    move_position = int(math.log(action,2))
+    table[first_player_board][second_player_board][move_position] = new_value
+    return table
 
 
   def deepcopy(self):
