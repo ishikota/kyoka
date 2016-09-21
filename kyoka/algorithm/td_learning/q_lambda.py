748036964d9a4e269eec32e1e9bce224f527fe53
Kota Ishimoto <ishikota086@gmail.com>
2016-09-21 16:06:34 +0900

    Add Q(lambda) algorithm

---
+++ kyoka/algorithm/td_learning/q_lambda.py
@@ -0,0 +1,64 @@
+from kyoka.algorithm.base_rl_algorithm import BaseRLAlgorithm
+from kyoka.algorithm.policy.greedy_policy import GreedyPolicy
+from kyoka.algorithm.td_learning.eligibility_trace.action_eligibility_trace\
+    import ActionEligibilityTrace as EligibilityTrace
+
+class QLambda(BaseRLAlgorithm):
+
+  ACTION_ON_TERMINAL_FLG = "action_on_terminal"
+
+  def __init__(self, alpha=0.1, gamma=0.9, eligibility_trace=None):
+    self.alpha = alpha
+    self.gamma = gamma
+    self.trace = eligibility_trace if eligibility_trace else self.__generate_default_trace()
+
+  def update_value_function(self, domain, policy, value_function):
+    greedy_policy = GreedyPolicy(domain, value_function)
+    current_state = domain.generate_initial_state()
+    current_action = policy.choose_action(current_state)
+    update_delta_history = []
+    while not domain.is_terminal_state(current_state):
+      next_state = domain.transit_state(current_state, current_action)
+      reward = domain.calculate_reward(next_state)
+      next_action = self.__choose_action(domain, policy, next_state)
+      greedy_action = self.__choose_action(domain, greedy_policy, next_state)
+      delta = self.__calculate_delta(value_function,\
+          current_state, current_action, next_state, greedy_action, reward)
+      self.trace.update(current_state, current_action)
+      for state, action, eligibility in self.trace.get_eligibilities():
+        new_Q_value = self.__calculate_new_Q_value(\
+            value_function, state, action, eligibility, delta)
+        update_delta = value_function.update_function(state, action, new_Q_value)
+        update_delta_history.append(update_delta)
+        self.trace.decay(state, action)
+      if greedy_action != next_action:
+        self.trace.clear()
+      current_state, current_action = next_state, next_action
+    return update_delta_history
+
+  def __calculate_delta(self,\
+      value_function, state, action, next_state, greedy_action, reward):
+    Q_value = value_function.calculate_value(state, action)
+    greedy_Q_value = self.__calculate_value(value_function, next_state, greedy_action)
+    return reward + self.gamma * greedy_Q_value - Q_value
+
+  def __calculate_new_Q_value(self,\
+      value_function, state, action, eligibility, delta):
+    Q_value = value_function.calculate_value(state, action)
+    return Q_value + self.alpha * delta * eligibility
+
+  def __choose_action(self, domain, policy, state):
+    if domain.is_terminal_state(state):
+      return self.ACTION_ON_TERMINAL_FLG
+    else:
+      return policy.choose_action(state)
+
+  def __calculate_value(self, value_function, next_state, next_action):
+    if self.ACTION_ON_TERMINAL_FLG == next_action:
+      return 0
+    else:
+      return value_function.calculate_value(next_state, next_action)
+
+  def __generate_default_trace(self):
+    return EligibilityTrace(EligibilityTrace.TYPE_ACCUMULATING)
+
---
+++ tests/kyoka/algorithm/td_learning/q_lambda_test.py
@@ -0,0 +1,67 @@
+from tests.base_unittest import BaseUnitTest
+from kyoka.algorithm.td_learning.q_lambda import QLambda
+from kyoka.algorithm.value_function.base_table_action_value_function import BaseTableActionValueFunction
+from kyoka.algorithm.policy.base_policy import BasePolicy
+from kyoka.algorithm.td_learning.eligibility_trace.action_eligibility_trace\
+    import ActionEligibilityTrace as EligibilityTrace
+
+from mock import Mock
+
+class QLambaTest(BaseUnitTest):
+
+  def setUp(self):
+    trace = EligibilityTrace(EligibilityTrace.TYPE_ACCUMULATING,\
+        discard_threshold=0.0001+0.0001, gamma=0.1, lambda_=0.1)
+    self.algo = QLambda(alpha=0.5, gamma=0.1, eligibility_trace=trace)
+
+  def test_update_value_function(self):
+    domain = self.__setup_stub_domain()
+    value_func = self.TestTableValueFunctionImpl()
+    value_func.setUp()
+    value_func.update_function(1, 2, 10)
+    value_func.update_function(1, 3, 11)
+    value_func.update_function(4, 5, 10)
+    value_func.update_function(4, 6, 100)
+    value_func.update_function(10, 11, 1000)
+    value_func.update_function(10, 12, 10000)
+    policy = self.CheetPolicyImple(domain, value_func)
+    delta = self.algo.update_value_function(domain, policy, value_func)
+    self.almosteq([-279.5, 0.075, 1.05, 5, 7.5, 500], sorted(delta), tolerance=0.0001)
+    expected = [(0, 1, 1.125), (1, 3, 23.5), (4, 6, 600), (10, 11, 720.5)]
+    for state, action, value in expected:
+      self.eq(value, value_func.fetch_value_from_table(value_func.table, state, action))
+
+
+  def __setup_stub_domain(self):
+    mock_domain = Mock()
+    mock_domain.generate_initial_state.return_value = 0
+    mock_domain.is_terminal_state.side_effect = lambda state: state == 21
+    mock_domain.transit_state.side_effect = lambda state, action: state + action
+    mock_domain.generate_possible_actions.side_effect = lambda state: [] if state == 21 else [state + 1, state + 2]
+    mock_domain.calculate_reward.side_effect = lambda state: state**2
+    return mock_domain
+
+  class TestTableValueFunctionImpl(BaseTableActionValueFunction):
+
+    def generate_initial_table(self):
+      return [[0 for j in range(13)] for i in range(11)]
+
+    def fetch_value_from_table(self, table, state, action):
+      table[state][action]
+      return table[state][action]
+
+    def update_table(self, table, state, action, new_value):
+      table[state][action] = new_value
+      return table
+
+  class CheetPolicyImple(BasePolicy):
+
+    def choose_action(self, state):
+      state_action_map = {
+          0: 1,
+          1: 3,
+          4: 6,
+          10: 11
+      }
+      return state_action_map[state]
+
