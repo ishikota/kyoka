04f9e35ff6c3b853471e37d598cd96ee3fd68789
Kota Ishimoto <ishikota086@gmail.com>
2016-10-05 00:10:27 +0900

    Log policy in each GPI iteration

--- sample/maze/maze_performance_logger.py
+++ sample/maze/maze_performance_logger.py
@@ -5,8 +5,10 @@ class MazePerformanceLogger(BaseCallback):
 
   def before_gpi_start(self, domain, value_function):
     self.step_log = []
+    self.policy_log = []
 
   def after_update(self, iteration_count, domain, value_function, delta):
     step_to_goal = MazeHelper.measure_performance(domain, value_function)
     self.step_log.append(step_to_goal)
+    self.policy_log.append(MazeHelper.visualize_policy(domain, value_function))
 
