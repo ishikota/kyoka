319f7bc68adf8cc148e59301635865e01b6b8d88
Kota Ishimoto <ishikota086@gmail.com>
2016-10-19 12:23:30 +0900

    Support StateValueFunction in MonteCarlo

--- kyoka/algorithm/montecarlo/montecarlo.py
+++ kyoka/algorithm/montecarlo/montecarlo.py
@@ -1,4 +1,6 @@
 from kyoka.algorithm.base_rl_algorithm import BaseRLAlgorithm
+from kyoka.value_function.base_action_value_function import BaseActionValueFunction
+from kyoka.value_function.base_state_value_function import BaseStateValueFunction
 from kyoka.value_function.base_table_action_value_function import BaseTableActionValueFunction
 from kyoka.value_function.base_table_state_value_function import BaseTableStateValueFunction
 
@@ -12,13 +14,12 @@ class MonteCarlo(BaseRLAlgorithm):
     update_counter = value_function.get_additinal_data(self.__KEY_ADDITIONAL_DATA)
     episode = self.generate_episode(domain, policy)
     for idx, turn_info in enumerate(episode):
-      state, action, _next_state, _reward = turn_info
-      Q_value = value_function.calculate_value(state, action)
-      update_count = value_function.fetch_value_from_table(update_counter, state, action)
-      following_reward = self.__calculate_following_state_reward(idx, episode)
-      new_Q_value = self.__calculate_new_Q_value(Q_value, update_count, following_reward)
-      value_function.update_table(update_counter, state, action, update_count + 1)
-      value_function.update_function(state, action, new_Q_value)
+      if isinstance(value_function, BaseActionValueFunction):
+        self.__update_action_value_function(\
+                domain, policy, value_function, update_counter, episode, idx, turn_info)
+      elif isinstance(value_function, BaseStateValueFunction):
+        self.__update_state_value_function(\
+                domain, policy, value_function, update_counter, episode, idx, turn_info)
     value_function.set_additinal_data(self.__KEY_ADDITIONAL_DATA, update_counter)
 
   def __validate_value_function(self, value_function):
@@ -28,13 +29,33 @@ class MonteCarlo(BaseRLAlgorithm):
       raise TypeError(self.__build_type_error_message())
 
   def __build_type_error_message(self):
-    return 'MonteCarlo method requires you to use "table" type action-value function.\
-        (child class of [BaseTableActionValueFunction])'
+    return 'MonteCarlo method requires you to use "table" type function.\
+        (child class of [BaseTableStateValueFunction or BaseTableActionValueFunction])'
 
   def __initialize_update_counter_if_needed(self, value_function):
     if value_function.get_additinal_data(self.__KEY_ADDITIONAL_DATA) is None:
       update_counter = value_function.generate_initial_table()
       value_function.set_additinal_data(self.__KEY_ADDITIONAL_DATA, update_counter)
+
+  def __update_action_value_function(\
+          self, domain, policy, value_function, update_counter, episode, idx, turn_info):
+      state, action, _next_state, _reward = turn_info
+      Q_value = value_function.calculate_value(state, action)
+      update_count = value_function.fetch_value_from_table(update_counter, state, action)
+      following_reward = self.__calculate_following_state_reward(idx, episode)
+      new_Q_value = self.__calculate_new_Q_value(Q_value, update_count, following_reward)
+      value_function.update_table(update_counter, state, action, update_count + 1)
+      value_function.update_function(state, action, new_Q_value)
+
+  def __update_state_value_function(\
+          self, domain, policy, value_function, update_counter, episode, idx, turn_info):
+      state, action, next_state, reward = turn_info
+      Q_value = value_function.calculate_value(next_state)
+      update_count = value_function.fetch_value_from_table(update_counter, next_state)
+      following_reward = self.__calculate_following_state_reward(idx, episode)
+      new_Q_value = self.__calculate_new_Q_value(Q_value, update_count, following_reward)
+      value_function.update_table(update_counter, next_state, update_count + 1)
+      value_function.update_function(next_state, new_Q_value)
 
   def __calculate_following_state_reward(self, current_turn, episode):
     following_turn_info = episode[current_turn:]
