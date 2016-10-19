cdbe77be0083dc78419f8a3a8169e313c4f0df61
Kota Ishimoto <ishikota086@gmail.com>
2016-10-19 17:41:33 +0900

    Create ExperienceReplay class

---
+++ kyoka/algorithm/experience_replay/experience_replay.py
@@ -0,0 +1,16 @@
+import random
+
+class ExperienceReplay(object):
+
+  def __init__(self, max_size):
+    self.max_size = max_size
+    self.queue = []
+
+  def store_transition(self, state, action, reward, next_state):
+    if len(self.queue) >= self.max_size:
+      self.queue.pop(0)
+    self.queue.append((state, action, reward, next_state))
+
+  def sample_minibatch(self, minibatch_size):
+    return random.sample(self.queue, minibatch_size)
+
