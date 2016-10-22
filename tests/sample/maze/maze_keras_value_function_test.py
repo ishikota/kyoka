fee0b6f992901fb7c7a1c7f8266749fa1bde23cc
Kota Ishimoto <ishikota086@gmail.com>
2016-10-22 14:06:47 +0900

    Disable keras depending test which will be removed

--- tests/sample/maze/maze_keras_value_function_test.py
+++ tests/sample/maze/maze_keras_value_function_test.py
@@ -1,6 +1,6 @@
 import os
 from tests.base_unittest import BaseUnitTest
-from sample.maze.maze_keras_value_function import MazeKerasValueFunction
+#from sample.maze.maze_keras_value_function import MazeKerasValueFunction
 from sample.maze.maze_domain import MazeDomain
 
 class MazeKerasValueFunctionTest(BaseUnitTest):
@@ -11,14 +11,14 @@ class MazeKerasValueFunctionTest(BaseUnitTest):
     self.func = MazeKerasValueFunction(self.domain)
     self.func.setUp()
 
-  def test_transform_state_action_into_input(self):
+  def xtest_transform_state_action_into_input(self):
     test_func = lambda state, action: self.func.transform_state_action_into_input(state, action)
     state1, action1 = (0, 0), self.domain.RIGHT
     self.eq([0,1,0,0,0,0,0,0,0,0,0,0], test_func(state1, action1))
     state2, action2 = (2, 1), self.domain.UP
     self.eq([0,0,0,0,0,1,0,0,0,0,0,0], test_func(state2, action2))
 
-  def _test_prediction(self):
+  def xtest_prediction(self):
     state, action = (0, 0), self.domain.DOWN
     delta = self.func.update_function(state, action, 1)
     res = self.func.calculate_value(state, action)
--- tests/sample/ticktacktoe/ticktacktoe_keras_value_function_test.py
+++ tests/sample/ticktacktoe/ticktacktoe_keras_value_function_test.py
@@ -1,13 +1,13 @@
 from tests.base_unittest import BaseUnitTest
-from sample.ticktacktoe.ticktacktoe_keras_value_function import TickTackToeKerasValueFunction
+#from sample.ticktacktoe.ticktacktoe_keras_value_function import TickTackToeKerasValueFunction
 
-class TickTackToeKerasValueFunctionTest(BaseUnitTest):
+class TickTackToeKerasValueFunctioneXtest(BaseUnitTest):
 
   def setUp(self):
     self.func = TickTackToeKerasValueFunction()
     self.func.setUp()
 
-  def test_state_action_into_input(self):
+  def xtest_state_action_into_input(self):
     bin2i = lambda b: int(b, 2)
     first_player_board = bin2i("000000100")
     second_player_board = bin2i("000000010")
@@ -15,7 +15,7 @@ class TickTackToeKerasValueFunctionTest(BaseUnitTest):
     model_input = self.func.transform_state_action_into_input((first_player_board, second_player_board), action)
     self.eq([1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0], model_input)
 
-  def _test_prediction(self):
+  def xtest_prediction(self):
     bin2i = lambda b: int(b, 2)
     first_player_board = bin2i("000000100")
     second_player_board = bin2i("000000010")
