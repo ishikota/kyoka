ac1bcbbc8c61254c3290f2d93bd00aaad0381233
Kota Ishimoto <ishikota086@gmail.com>
2016-11-09 14:59:01 +0900

    Fix approx value function interface

--- kyoka/value_function_.py
+++ kyoka/value_function_.py
@@ -1,6 +1,14 @@
 from kyoka.utils import build_not_implemented_msg
 
-class BaseValueFunction(object):
+class BaseActionValueFunction(object):
+
+    def predict_value(self, state, action):
+        err_msg = build_not_implemented_msg(self, "predict_value")
+        raise NotImplementedError(err_msg)
+
+    def backup(self, state, action, backup_target, alpha):
+        err_msg = build_not_implemented_msg(self, "backup")
+        raise NotImplementedError(err_msg)
 
     def setup(self):
       pass
@@ -11,23 +19,28 @@ class BaseValueFunction(object):
     def load(self, load_dir_path):
       pass
 
-class BaseTabularActionValueFunction(BaseValueFunction):
+
+class BaseTabularActionValueFunction(BaseActionValueFunction):
+    pass
+
+
+class BaseApproxActionValueFunction(BaseActionValueFunction):
 
     def predict_value(self, state, action):
-        err_msg = build_not_implemented_msg(self, "predict_value")
-        raise NotImplementedError(err_msg)
+        return self.approx_predict_value(self.construct_features(state, action))
 
     def backup(self, state, action, backup_target, alpha):
-        err_msg = build_not_implemented_msg(self, "backup")
+        self.approx_backup(self.construct_features(state, action), backup_target, alpha)
+
+    def construct_features(self, state, action):
+        err_msg = build_not_implemented_msg(self, "construct_features")
         raise NotImplementedError(err_msg)
 
-class BaseApproxActionValueFunction(BaseValueFunction):
-
-    def predict_value(self, features):
-        err_msg = build_not_implemented_msg(self, "predict_value")
+    def approx_predict_value(self, features):
+        err_msg = build_not_implemented_msg(self, "approx_predict_value")
         raise NotImplementedError(err_msg)
 
-    def backup(self, features, backup_target, alpha):
-        err_msg = build_not_implemented_msg(self, "backup")
+    def approx_backup(self, features, backup_target, alpha):
+        err_msg = build_not_implemented_msg(self, "approx_backup")
         raise NotImplementedError(err_msg)
 
--- tests/kyoka/value_function_test.py
+++ tests/kyoka/value_function_test.py
@@ -20,11 +20,35 @@ class BaseApproxActionValueFunctionTest(BaseUnitTest):
     def setUp(self):
         self.func = BaseApproxActionValueFunction()
 
-    @raises(NotImplementedError)
-    def test_error_msg_when_not_implement_calculate_value(self):
-        self.func.predict_value("dummy")
+    def test_predoct_value(self):
+        func = self.TestImpl()
+        self.eq("predict:hogefuga", func.predict_value("hoge", "fuga"))
+
+    def test_backup(self):
+        func = self.TestImpl()
+        func.backup("hoge", "fuga", "dummy", "dummy")
+        self.eq("backup:hogefuga", func.memo)
 
     @raises(NotImplementedError)
-    def test_error_msg_when_not_implement_update_function(self):
-        self.func.backup("dummy", "dummy", "dummy")
+    def test_error_msg_when_not_implement_construct_features(self):
+        self.func.construct_features("dummy", "dummy")
+
+    @raises(NotImplementedError)
+    def test_error_msg_when_not_implement_approx_predict_value(self):
+        self.func.approx_predict_value("dummy")
+
+    @raises(NotImplementedError)
+    def test_error_msg_when_not_implement_approx_backup(self):
+        self.func.approx_backup("dummy", "dummy", "dummy")
+
+    class TestImpl(BaseApproxActionValueFunction):
+
+        def construct_features(self, state, action):
+            return state + action
+
+        def approx_predict_value(self, features):
+            return "predict:%s" % features
+
+        def approx_backup(self, features, backup_target, alpha):
+            self.memo = "backup:%s" % features
 
