e66693091345ee484a5f4586a2620a4153745073
Kota Ishimoto <ishikota086@gmail.com>
2016-11-10 10:47:39 +0900

    Add Sarsa algorithm

---
+++ tests/kyoka/algorithm_/sarsa_test.py
@@ -0,0 +1,60 @@
+import os
+
+from tests.base_unittest import BaseUnitTest
+from tests.utils import generate_tmp_dir_path, setup_tmp_dir, teardown_tmp_dir, NegativePolicy
+from mock import Mock
+
+from kyoka.algorithm_.sarsa import Sarsa, SarsaTabularActionValueFunction
+
+class SarsaTest(BaseUnitTest):
+
+    def test_run_gpi_for_an_episode(self):
+        algo = Sarsa(alpha=0.5, gamma=0.1)
+        task = setup_stub_task()
+        value_func = SarsaTabularActionValueFunctionImpl()
+        policy = NegativePolicy()
+        algo.setup(task, policy, value_func)
+
+        value_func.insert_value_into_table(value_func.table, 1, 2, 10)
+        value_func.insert_value_into_table(value_func.table, 1, 3, 11)
+        value_func.insert_value_into_table(value_func.table, 3, 4, 100)
+        value_func.insert_value_into_table(value_func.table, 3, 5, 101)
+
+        algo.run_gpi_for_an_episode(task, policy, value_func)
+
+        expected = [(0, 1, 1), (1, 2, 14.5), (3, 4, 74.5)]
+        for state, action, value in expected:
+            self.eq(value, value_func.predict_value(state, action))
+
+
+class SarsaTabularActionValueFunctionTest(BaseUnitTest):
+
+    def test_backup(self):
+        func = SarsaTabularActionValueFunctionImpl()
+        func.setup()
+        func.backup(state=0, action=0, backup_target=2, alpha=0.5)
+        self.eq(1, func.predict_value(0, 0))
+        func.backup(state=0, action=0, backup_target=2, alpha=0.5)
+        self.eq(1.5, func.predict_value(0, 0))
+
+
+class SarsaTabularActionValueFunctionImpl(SarsaTabularActionValueFunction):
+
+    def generate_initial_table(self):
+        return [[0 for j in range(50)] for i in range(8)]
+
+    def fetch_value_from_table(self, table, state, action):
+        return table[state][action]
+
+    def insert_value_into_table(self, table, state, action, new_value):
+        table[state][action] = new_value
+
+def setup_stub_task():
+    mock_task = Mock()
+    mock_task.generate_initial_state.return_value = 0
+    mock_task.is_terminal_state.side_effect = lambda state: state == 7
+    mock_task.transit_state.side_effect = lambda state, action: state + action
+    mock_task.generate_possible_actions.side_effect = lambda state: [] if state == 7 else [state + 1, state + 2]
+    mock_task.calculate_reward.side_effect = lambda state: state**2
+    return mock_task
+
--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,7 @@
+from __future__ import absolute_import
+
 import os
+from kyoka.policy_ import BasePolicy
 
 def generate_tmp_dir_path(script_path):
     return os.path.join(os.path.dirname(script_path), "tmp")
@@ -13,4 +16,14 @@ def teardown_tmp_dir(script_path, file_names):
             file_path = os.path.join(dir_path, file_name)
             if os.path.exists(file_path): os.remove(file_path)
         os.rmdir(dir_path)
+
+class NegativePolicy(BasePolicy):
+
+    def choose_action(self, task, value_function, state):
+        actions = task.generate_possible_actions(state)
+        Q_value_for_actions = [value_function.predict_value(state, action) for action in actions]
+        min_Q_value = min(Q_value_for_actions)
+        Q_act_pair = zip(Q_value_for_actions, actions)
+        worst_actions = [act for Q_value, act in Q_act_pair if min_Q_value == Q_value]
+        return worst_actions[0]
 
