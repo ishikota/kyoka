2edd111afa7e16f3db7eda799cc60668e5143005
Kota Ishimoto <ishikota086@gmail.com>
2016-11-24 15:03:54 +0900

    Improve mcts sample of ticktacktoe

--- sample/ticktacktoe/script/play_with_agent/montecarlo_tree_search.py
+++ sample/ticktacktoe/script/play_with_agent/montecarlo_tree_search.py
@@ -13,6 +13,7 @@ sys.path.append(src_path)
 sys.path.append(sample_path)
 
 import sample.ticktacktoe.helper as Helper
+from sample.ticktacktoe.callback import TickTackToePerfectPolicy
 from sample.ticktacktoe.helper import TickTackToeManualPolicy
 from sample.ticktacktoe.task import TickTackToeTask
 
@@ -38,23 +39,38 @@ class MCTS(BaseMCTS):
     def next_is_first_player(self, state):
         return bin(state[0]|state[1]).count("1") % 2 == 0
 
-SIMULATION_NUM = 1000
-finish_rule = WatchIterationCount(SIMULATION_NUM)
+TEST_NUM = 10
+SIMULATION_NUM = 5000
+finish_rule = WatchIterationCount(SIMULATION_NUM, verbose=0)
 next_is_first_player = lambda state: bin(state[0]|state[1]).count("1") % 2 == 0
 def show_board(state):
     print "\n%s" % Helper.visualize_board(state)
 
-task = TickTackToeTask(is_first_player=False)
 algo = MCTS(TickTackToeTask(is_first_player=True))
 algo.set_finish_rule(finish_rule)
-human = TickTackToeManualPolicy()
-state = TickTackToeTask().generate_initial_state()
-show_board(state)
-while not task.is_terminal_state(state):
-    if next_is_first_player(state):
-        action = algo.choose_action(None, None, state)
-    else:
-        action = human.choose_action(task, None, state)
-    state = task.transit_state(state, action)
+
+# Measure performance
+minimax = TickTackToePerfectPolicy()
+players = [algo, minimax]
+game_results = [Helper.measure_performance(TickTackToeTask(True), None, players) for _ in range(TEST_NUM)]
+result_count = [game_results.count(result) for result in [-1, 0, 1]]
+result_rate  = [1.0 * count / len(game_results) for count in result_count]
+print "VS PerfectPolicy average result: lose=%f, draw=%f, win=%f" % tuple(result_rate)
+
+# Play with agent
+flg = raw_input("Do you want to play with trained agent? (y/n) >> ")
+fetch_action_value = lambda tree: [edge.calculate_value() for edge in tree.child_edges]
+if flg in ["y", "yes"]:
+    task = TickTackToeTask(is_first_player=False)
+    human = TickTackToeManualPolicy()
+    state = TickTackToeTask().generate_initial_state()
     show_board(state)
+    while not task.is_terminal_state(state):
+        if next_is_first_player(state):
+            action = algo.choose_action(None, None, state)
+            print "[Action Values] %s" % fetch_action_value(algo.last_calculated_tree)
+        else:
+            action = human.choose_action(task, None, state)
+        state = task.transit_state(state, action)
+        show_board(state)
 
