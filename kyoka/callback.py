1b2bdcf0d98a535744063daeb7ebac6bcd95adf1
Kota Ishimoto <ishikota086@gmail.com>
2016-12-02 12:14:56 +0900

    Fix indent

--- kyoka/callback.py
+++ kyoka/callback.py
@@ -6,27 +6,27 @@ from utils import build_not_implemented_msg
 
 class BaseCallback(object):
 
-  def before_gpi_start(self, task, value_function):
-    pass
+    def before_gpi_start(self, task, value_function):
+        pass
 
-  def before_update(self, iteration_count, task, value_function):
-    pass
+    def before_update(self, iteration_count, task, value_function):
+        pass
 
-  def after_update(self, iteration_count, task, value_function):
-    pass
+    def after_update(self, iteration_count, task, value_function):
+        pass
 
-  def after_gpi_finish(self, task, value_function):
-    pass
+    def after_gpi_finish(self, task, value_function):
+        pass
 
-  def interrupt_gpi(self, iteration_count, task, value_function):
-    return False
+    def interrupt_gpi(self, iteration_count, task, value_function):
+        return False
 
-  def define_log_tag(self):
-    return self.__class__.__name__
+    def define_log_tag(self):
+        return self.__class__.__name__
 
-  def log(self, message):
-    if message and len(message) != 0:
-      print "[%s] %s" % (self.define_log_tag(), message)
+    def log(self, message):
+        if message and len(message) != 0:
+            print "[%s] %s" % (self.define_log_tag(), message)
 
 class BasePerformanceWatcher(BaseCallback):
 
